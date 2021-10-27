3dc48c7928eca0b24f2b5ceceb23eb3d5440b670
JackRyan1989 <jack.jackryan@gmail.com>
2021-10-27 07:57:53 -0400

    Updated service worker and manifest

--- src/sw.js
+++ src/sw.js
@@ -2,14 +2,14 @@
 
 const cacheName = 'noteTaker-v1';
 const appShellFiles = [
-    '/notetaker/public/',
-    '/notetaker/public/index.html',
-    '/notetaker/public/app.js',
-    '/notetaker/public/index.css'
+    '/notetaker/dist/',
+    '/notetaker/dist/index.html',
+    '/notetaker/dist/app.js',
+    '/notetaker/dist/index.css',
+    '/dist/assets/logo/'
 ];
 
 self.addEventListener('install', (e) => {
-    console.log('Service Worker Install');
     e.waitUntil((async ()=> {
         const cache = await caches.open(cacheName);
         await cache.addAll(appShellFiles);
