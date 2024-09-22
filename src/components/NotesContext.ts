751906697e814debf53f38d98728d39a09634aac
JackRyan1989 <jack.jackryan@gmail.com>
2024-09-21 20:55:29 -0400

    get rid of test notes

--- src/components/NotesContext.ts
+++ src/components/NotesContext.ts
@@ -6,25 +6,6 @@ export interface NotesContextType {
     setNotes: React.Dispatch<React.SetStateAction<Notes>>
   }
 
-  const testNotes: Notes = [
-    {
-      title: 'A singular man.',
-      content: 'An unequivocal voice found me alone, in the tub. Listening to whales on acid.',
-      createdOn: new Date(),
-      updatedOn: null,
-      id: 1
-    },
-    {
-      title: 'A doubular person.',
-      content: 'An unequivocal voice found me alone, in the tub. Listening to acid on whales.',
-      createdOn: new Date(),
-      updatedOn: null,
-      id: 2
-    }
-  ]
-
 const NotesContext = createContext<any | null>(null);
 
-
-export { testNotes }
 export default NotesContext
